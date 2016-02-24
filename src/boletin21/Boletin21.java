7432af66a0fdeb58bdc27699c887b10c0a720760
igonzalezcastro <igonzalezcastro@atanasoff21.danielcastelao.org>
2016-02-24 11:20:04 +0100

    Commit

---
+++ src/boletin21/Boletin21.java
@@ -0,0 +1,15 @@
+package boletin21;
+
+/**
+ *
+ * @author igonzalezcastro
+ */
+public class Boletin21 {
+
+    public static void main(String[] args) {
+        ordenarPalabras obx = new ordenarPalabras();
+        obx.cargarArray();
+        obx.guardarFichero();
+    }
+
+}
